34eafee31f4513d80e3aa5cea00f07f1dbbb8b61
Skygut <v.chub@outlook.de>
2025-04-09 21:55:18 +0200

    update

--- README.md
+++ README.md
@@ -7,15 +7,27 @@ https://www.youtube.com/watch?v=EyzRixV8s54
 https://github.com/coqui-ai/TTS?tab=readme-ov-file
 
 #  створити віртуальне середовище
+source - там де знаходиться папка
+source .venv/bin/activate
+
 # Створюємо папку outputs
+якщо потрібно деактивувати віртуальне середовище:
+deactivate
 
 # Наступні команди:
 
+pip install torch
+pip install gradio
+pip install TTS
+
 pip list | grep TTS
+
+
+ 
 
 tts --list_models
 
-pip install gradio
+
 
 Запускаємо python main.py
 та переходимо на посилання в терміналі:
@@ -23,3 +35,21 @@ pip install gradio
 * Running on local URL:  http://00000000000
 
 ![alt text](image-1.png)
+
+
+<!-- 1.Щоб перевірити, в якій ти зараз знаходишся папці (тобто "де я є у файловій системі"), просто введи в терміналі: -->
+pwd
+
+<!-- Це скорочення від print working directory, і воно покаже повний шлях до поточної папки. -->
+
+<!-- 2. Додатково: показати список файлів у цій папці -->
+ls або ls -la 
+<!-- Це допомагає перевірити, чи є там, наприклад, віртуальне середовище або main.py. -->
+
+<!-- 3. Якщо хочеш перейти до іншої папки, використай: -->
+
+cd /шлях/до/папки
+
+<!-- Наприклад: -->
+
+cd ~/Documents/GitHub/Text-to-speech
--- main.py
+++ main.py
@@ -5,9 +5,38 @@ import gradio as gr
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
+# def generate_audio(text="A journey of a thousand miles begins with a single step."):
+#     tts = TTS(model_name="tts_models/en/ljspeech/fast_pitch").to(device)
+#     tts.tts_to_file(text=text, file_path="outputs/output.wav")
+#     return "outputs/output.wav"
+
+
+# def generate_audio(text="A journey of a thousand miles begins with a single step."):
+#     tts = TTS(model_name="tts_models/en/vctk/vits").to(device)
+#     tts.tts_to_file(text=text, speaker="p225", file_path="outputs/output.wav")
+#     return "outputs/output.wav"
+
+
+# def generate_audio(text="A journey of a thousand miles begins with a single step."):
+#     tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts").to(device)
+#     tts.tts_to_file(
+#         text=text, speaker="male-en-1", language="en", file_path="outputs/output.wav"
+#     )
+#     return "outputs/output.wav"
+
+
 def generate_audio(text="A journey of a thousand miles begins with a single step."):
-    tts = TTS(model_name="tts_models/en/ljspeech/fast_pitch").to(device)
-    tts.tts_to_file(text=text, file_path="outputs/output.wav")
+    tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts").to("cpu")
+
+    # Beispiel: einen gültigen Speaker ausgeben
+    print("Speakers:", tts.speakers)
+
+    tts.tts_to_file(
+        text=text,
+        speaker="male-pt-3\n",  # Hier durch gültigen Namen ersetzen
+        language="en",
+        file_path="outputs/output.wav",
+    )
     return "outputs/output.wav"
 
 
